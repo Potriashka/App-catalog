ffaf12ee29471a40516e92949bcd268ba3698498
Potriashka <54754814+Potriashka@users.noreply.github.com>
2020-08-18 23:43:18 +0300

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,8 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
 from firebase import firebase
 
 app = Flask(__name__)
+app.secret_key = 'secret_key'
 
 firebase = firebase.FirebaseApplication("https://my-awesome-project-46dd8.firebaseio.com/", None)
 
@@ -13,56 +14,100 @@ def reallogin():
 		data = {
 			'Username':username,
 			'Password':password
-			}
-		result = firebase.post('my-awesome-project-46dd8/Customer', data)
+		}
+		result = firebase.post('my-awesome-project-46dd8/User', data)
 		print(result)
+		session["username"] = username
+		session["password"] = password
 		return redirect(url_for("home"))
 	else:
 		return render_template('login.html')
 
+@app.route('/data')
+def data1():
+	if "username" in session:
+		return render_template('search.html', username=session["username"], password = session["password"])
+	else: 
+		return redirect(url_for("reallogin"))
+
 @app.route('/games')
 def games():
-    return render_template('games.html')
+	if "username" in session:
+		return render_template('games.html')
+	else: 
+		return redirect(url_for("reallogin"))
 
 @app.route('/apps')
 def apps():
-    return render_template('apps.html')
+	if "username" in session:
+		return render_template('apps.html')
+	else: 
+		return redirect(url_for("reallogin"))
+
+@app.route('/logout')
+def logout():
+	session.pop("username", None)
+	session.pop("password", None)
+	return redirect(url_for("home"))
 
 @app.route('/sites')
 def sites():
-    return render_template('sites.html')
+	if "username" in session:
+		return render_template('sites.html')
+	else: 
+		return redirect(url_for("reallogin"))
 
 @app.route('/about-us')
 def aboutus():
-    return render_template('contacts.html')
+	if "username" in session:
+		return render_template('contacts.html')
+	else: 
+		return redirect(url_for("reallogin"))
 
 @app.route('/peter')
 def peter():
-    return render_template('peter.html')
+	if "username" in session:
+		return render_template('peter.html')
+	else: 
+		return redirect(url_for("reallogin"))
 
 @app.route('/bekhruz')
 def bekhruz():
-    return render_template('bekhruz.html')
+	if "username" in session:
+		return render_template('bekhruz.html')
+	else: 
+		return redirect(url_for("reallogin"))
 
 @app.route('/user')
 def user123():
-	return f"{username}<br/>{password}"
+	if "username" in session:
+		return f"{username}<br/>{password}"
+	else: 
+		return redirect(url_for("reallogin"))
 
 @app.route('/social-app')
 def social_app():
-	return render_template('social-app.html')
+	if "username" in session:
+		return render_template('social-app.html')
+	else: 
+		return redirect(url_for("reallogin"))
 
 @app.route("/<usr>")
 def user(usr):
-    return f"<h1>{usr}</h1>"
+	if "username" in session:
+		return f"<h1>{usr}</h1>"
+	else:
+		return redirect(url_for("reallogin"))
 
 @app.route('/', methods=["POST", "GET"])
 def home():
-	if request.method == "POST":
-		user = request.form["search_field"]
-		return redirect(url_for("user", usr=user))
-	else:
-		return render_template('home.html')
+	if "username" in session:
+		if request.method == "POST":
+			user = request.form["search_field"]
+			return redirect(url_for("user", usr=user))
+		else:
+			return render_template('home.html')
+	else: return redirect(url_for("reallogin"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
